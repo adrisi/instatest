0841d94eaa3eb1b651d0e4e7bd64843c6cd4fad2
Jayson Martinez <jaysmartinez@gmail.com>
2017-07-02 15:55:29 -0400

    Secrets in ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,13 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  Instagram.configure do |config|
+    config.client_id = ENV["client_id"]
+    config.client_secret = ENV["client_secret"]
+    # For secured endpoints only
+    #config.client_ips = '<Comma separated list of IPs>'
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end

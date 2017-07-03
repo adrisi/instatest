e790bf4b8a319c49707ce8013b14eb8c06da9137
Jayson Martinez <jaysmartinez@gmail.com>
2017-07-03 18:48:29 -0400

    Adds Instagram api to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    dashboard_path
+    # dashboard_path
+    callback_url = "http://localhost:3000/oauth/callback"
+    Instagram.authorize_url(:redirect_uri => callback_url)
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    user_session_path
   end
 end
